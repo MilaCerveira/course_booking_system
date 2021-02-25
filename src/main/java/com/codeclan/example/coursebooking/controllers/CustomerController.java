8827a1631ae4122d223e6a65f68e0a465aa45e0d
mack millar <mackmillar@gmail.com>
2021-02-25 15:21:53 +0000

    extensions

--- src/main/java/com/codeclan/example/coursebooking/controllers/CustomerController.java
+++ src/main/java/com/codeclan/example/coursebooking/controllers/CustomerController.java
@@ -1,5 +1,6 @@
 package com.codeclan.example.coursebooking.controllers;
 
+import com.codeclan.example.coursebooking.models.Customer;
 import com.codeclan.example.coursebooking.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CustomerController {
 
@@ -15,11 +18,23 @@ public class CustomerController {
     CustomerRepository customerRepository;
 
     @GetMapping(value = "/customers")
-    public ResponseEntity getAllCustomersAndFilters (
-            @RequestParam(required = false, name = "name") String name
-    ) {
+    public ResponseEntity getAllCustomersAndFilters(
+            @RequestParam(required = false, name = "name") String name,
+            @RequestParam(required = false, name = "town") String town,
+            @RequestParam(required = false, name = "courseName") String courseName,
+            @RequestParam(required = false, name = "age") Integer age
+    ) { // if we have the course name then do the course query
         if (name != null) {
             return new ResponseEntity(customerRepository.findByBookingsCourseName(name), HttpStatus.OK);
+        }
+        // if we have the course town & the course name then do this
+        if (town != null && courseName != null) {
+            List<Customer> foundCustomer = customerRepository.findByTownAndBookingsCourseName(town, courseName);
+            return new ResponseEntity(foundCustomer, HttpStatus.OK);
+        }
+        if (age > 20 && town != null) {
+            List<Customer> foundCustomer = customerRepository.findByAgeAndTownAndBookingsCourseName(age, town, courseName);
+            return new ResponseEntity(foundCustomer, HttpStatus.OK);
         }
         return new ResponseEntity(customerRepository.findAll(), HttpStatus.OK);
     }
--- src/main/java/com/codeclan/example/coursebooking/repositories/CustomerRepository.java
+++ src/main/java/com/codeclan/example/coursebooking/repositories/CustomerRepository.java
@@ -6,12 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-    //Get all customers for a given course
-
-    // we want customers from a course
-
-    // we have a course findByBookingsCustomerName
+        //Get all customers over a certain age in a given town for a given course
 
     List<Customer> findByBookingsCourseName(String name);
+
+    List<Customer> findByTownAndBookingsCourseName(String town, String courseName);
+    List<Customer> findByAgeAndTownAndBookingsCourseName(Integer age, String town, String courseName);
 }
